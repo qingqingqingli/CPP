94cd5eb0417d0a7f36880eec155ba3e321219d8f
Qing Li <qli@student.codam.nl>
2020-11-13 14:45:57 +0100

    updated ex00

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qli <qli@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/13 13:52:15 by qli           #+#    #+#                 */
+/*   Updated: 2020/11/13 14:44:24 by qli           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int j;
+
+	i = 1;
+	j = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE. *" << std::endl;
+		return 0;
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			std::cout << (char)(toupper(argv[i][j]));
+			j++;
+		}
+		if (i != argc - 1)
+			std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
