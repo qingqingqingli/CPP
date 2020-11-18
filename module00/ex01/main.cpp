dcf29b5d8828ed33f7d09aa23eb6983d18582218
Qing Li <qli@student.codam.nl>
2020-11-18 14:39:52 +0100

    update structure

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: qli <qli@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/16 10:40:41 by qli           #+#    #+#                 */
-/*   Updated: 2020/11/17 17:25:07 by qli           ########   odam.nl         */
+/*   Updated: 2020/11/18 14:35:48 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ int main(void)
 	std::string		command;
 	Phonebook		contact[8];
 
-	while (1)
+	while (command.compare("EXIT"))
 	{
 		std::cout << BOLDYELLOW << "Enter your command: " << RESET;
 		std::getline(std::cin, command);
+		if (std::cin.eof())
+		{
+			std::cout << std::endl;
+			return (1);
+		}
 		if (command.compare("ADD") == 0)
 			Phonebook::addContact(contact[Phonebook::getContactNb()]);
 		else if (command.compare("SEARCH") == 0)
 			Phonebook::searchContact(contact);
-		else if (command.compare("EXIT") == 0)
-			return (0);
 		else
 			std::cout << RED << "Please enter: ADD, SEARCH or EXIT" << RESET << std::endl;
 	}
