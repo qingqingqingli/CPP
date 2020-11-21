78a62ba2715db9e82d38ded11b95ba7031cb1685
qingqingqingli <kadlqbetty@gmail.com>
2020-11-21 13:13:44 +0100

    update ex04

---
+++ module01/ex04/cmake-build-debug/CMakeFiles/ex04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex04.dir/ex04.cpp.o"
+  "ex04"
+  "ex04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ module01/ex04/ex04.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ex04.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qli <qli@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/21 13:07:22 by qli           #+#    #+#                 */
+/*   Updated: 2020/11/21 13:07:22 by qli           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main(){
+	std::string	str = "HI THIS IS BRAIN";
+
+	std::string*	strPtr = &str;
+	std::string&	strRef = str;
+
+	std::cout << "strPtr is " << *strPtr << std::endl;
+	std::cout << "strRef is " << strRef << std::endl;
+	return 0;
+}
