e0026be3185bf44f7f22818d6510abf6d53017b7
qingqingqingli <kadlqbetty@gmail.com>
2020-11-26 22:35:18 +0100

    added ex07

---
+++ module01/ex07/cmake-build-debug/CMakeFiles/ex07.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex07.dir/replace.cpp.o"
+  "ex07"
+  "ex07.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
