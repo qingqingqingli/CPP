deb2c1d7bc8fb3104fa84bae464cef4faf29397c
qingqingqingli <kadlqbetty@gmail.com>
2020-12-04 17:16:57 +0100

    add ex03

---
+++ module03/ex03/cmake-build-debug/CMakeFiles/ex03.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex03.dir/NinjaTrap.cpp.o"
+  "CMakeFiles/ex03.dir/main.cpp.o"
+  "ex03"
+  "ex03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
