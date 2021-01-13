923d965a1ab3e43bde520e33f66c1b547551d9d7
qingqingqingli <kadlqbetty@gmail.com>
2021-01-13 20:20:51 +0100

    updated main

--- module04/ex02/main.cpp
+++ module04/ex02/main.cpp
@@ -17,62 +17,63 @@
 #include "TacticalMarine.hpp"
 #include "AssaultTerminator.hpp"
 
-//int main(void)
-//{
-//	ISpaceMarine* bob = new TacticalMarine;
-//	ISpaceMarine* jim = new AssaultTerminator;
-//
-//	ISquad* vlc = new Squad;
-//	vlc->push(bob);
-//	vlc->push(jim);
-//	for (int i = 0; i < vlc->getCount(); ++i)
-//	{
-//		ISpaceMarine* cur = vlc->getUnit(i);
-//		cur->battleCry();
-//		cur->rangedAttack();
-//		cur->meleeAttack();
-//	}
-//	delete vlc;
-//
-//	return 0;
-//}
-
-int main()
+int main(void)
 {
-	// create one squad
-    Squad* original_squad = new Squad;
-    ISpaceMarine* bob = new TacticalMarine;
-    ISpaceMarine* jim = new AssaultTerminator;
+	{
+		ISpaceMarine* bob = new TacticalMarine;
+		ISpaceMarine* jim = new AssaultTerminator;
 
-    original_squad->push(bob);
-    original_squad->push(jim);
+		ISquad* vlc = new Squad;
+		vlc->push(bob);
+		vlc->push(jim);
+		for (int i = 0; i < vlc->getCount(); ++i)
+		{
+			ISpaceMarine* cur = vlc->getUnit(i);
+			cur->battleCry();
+			cur->rangedAttack();
+			cur->meleeAttack();
+		}
+		delete vlc;
+	}
+	std::cout << "---------" << std::endl;
+	{
+		// create one squad
+		Squad* original_squad = new Squad;
+		ISpaceMarine* bob = new TacticalMarine;
+		ISpaceMarine* jim = new AssaultTerminator;
 
-    // test copy constructor
-	Squad* copied_squad = new Squad(*original_squad);
-	ISpaceMarine* jerry = new TacticalMarine;
-	ISpaceMarine* jon = new AssaultTerminator;
+		original_squad->push(bob);
+		original_squad->push(jim);
 
-	copied_squad->push(jerry);
-	copied_squad->push(jon);
+		// test copy constructor
+		Squad* copied_squad = new Squad(*original_squad);
+		ISpaceMarine* jerry = new TacticalMarine;
+		ISpaceMarine* jon = new AssaultTerminator;
 
-	std::cout << BLUE << "original squad has: " << original_squad->getCount() << " units." << std::endl;
-	std::cout << "copied squad has: " << copied_squad->getCount() << " units." << RESET << std::endl;
+		copied_squad->push(jerry);
+		copied_squad->push(jon);
 
-    //test assignation operator
-	std::cout << "---" << std::endl;
-	*copied_squad = *original_squad;
+		std::cout << BLUE << "original squad has: " << original_squad->getCount() << " units." << std::endl;
+		std::cout << "copied squad has: " << copied_squad->getCount() << " units." << RESET << std::endl;
 
-	std::cout << BLUE << "original squad has: " << original_squad->getCount() << " units." << std::endl;
-	std::cout << "copied squad has: " << copied_squad->getCount() << " units." << RESET << std::endl;
+		//test assignation operator
+		std::cout << "---" << std::endl;
+		*copied_squad = *original_squad;
 
-    //delete the squads and their content
-	std::cout << "---" << std::endl;
-	delete original_squad;
-	delete copied_squad;
-    return 0;
+		std::cout << BLUE << "original squad has: " << original_squad->getCount() << " units." << std::endl;
+		std::cout << "copied squad has: " << copied_squad->getCount() << " units." << RESET << std::endl;
+
+		//delete the squads and their content
+		std::cout << "---" << std::endl;
+		delete original_squad;
+		delete copied_squad;
+	}
+	return 0;
 }
 
-// // <first main>
+
+// // <first part>
+
 // Tactical Marine ready for battle!$
 // * teleports from space *$
 // For the holy PLOT!$
@@ -84,7 +85,7 @@ int main()
 // Aaargh...$
 // I'll be back...$
 
-// // <second main>
+// // <second part>
 
 //Tactical Marine ready for battle!
 //* teleports from space *
--- module04/ex03/main.cpp
+++ module04/ex03/main.cpp
@@ -8,80 +8,105 @@
 #include "ICharacter.hpp"
 #include "AMateria.hpp"
 
-int main(){
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+int main()
+{
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
+		ICharacter* me = new Character("me");
 
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
 
-	ICharacter* bob = new Character("bob");
+		ICharacter* bob = new Character("bob");
 
-	me->use(0, *bob);
-	me->use(1, *bob);
+		me->use(0, *bob);
+		me->use(1, *bob);
 
-	delete bob;
-	delete me;
-	delete src;
+		delete bob;
+		delete me;
+		delete src;
+	}
+	std::cout << "---------" << std::endl;
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
+		Character* me = new Character("me");
+
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		Character* bob = new Character("bob");
+
+		//test copy constructor & assignation operator
+		Character *meCopy = new Character(*me);
+		std::cout << "----" << std::endl;
+		Character *bobCopy = new Character(*bob);
+		std::cout << "----" << std::endl;
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+
+		std::cout << "----" << std::endl;
+		meCopy->use(0, *bobCopy);
+		meCopy->use(1, *bobCopy);
+
+		//test unequip
+		std::cout << "----" << std::endl;
+
+		AMateria *materiaCopy = meCopy->getMateria(1);
+		meCopy->unequip(1);
+
+		meCopy->use(0, *bobCopy);
+		meCopy->use(1, *bobCopy);
+		me->use(0, *bob);
+		me->use(1, *bob);
+
+		delete src;
+		delete bob;
+		delete bobCopy;
+		delete me;
+		delete meCopy;
+		delete materiaCopy;
+	}
 	return 0;
 }
 
-//int main(){
-//	IMateriaSource* src = new MateriaSource();
-//	src->learnMateria(new Ice());
-//	src->learnMateria(new Cure());
-//
-//	Character* me = new Character("me");
-//
-//	AMateria* tmp;
-//	tmp = src->createMateria("ice");
-//	me->equip(tmp);
-//	tmp = src->createMateria("cure");
-//	me->equip(tmp);
-//
-//	Character* bob = new Character("bob");
-//
-//	//test copy constructor & assignation operator
-//	Character *meCopy = new Character(*me);
-//	std::cout << "----" << std::endl;
-//	Character *bobCopy = new Character(*bob);
-//	std::cout << "----" << std::endl;
-//
-//	me->use(0, *bob);
-//	me->use(1, *bob);
-//
-//	std::cout << "----" << std::endl;
-//	meCopy->use(0, *bobCopy);
-//	meCopy->use(1, *bobCopy);
-//
-//	//test unequip
-//	std::cout << "----" << std::endl;
-//
-//	AMateria *materiaCopy = meCopy->getMateria(1);
-//	meCopy->unequip(1);
-//
-//	meCopy->use(0, *bobCopy);
-//	meCopy->use(1, *bobCopy);
-//	me->use(0, *bob);
-//	me->use(1, *bob);
-//
-//	delete src;
-//	delete bob;
-//	delete bobCopy;
-//	delete me;
-//	delete meCopy;
-//	delete materiaCopy;
-//
-//	return 0;
-//}
+// // <first part>
 
 //* shoots an ice bolt at bob *
 //* heals bob's wounds *
 
+// // <second part>
+
+//Character copy constructor
+//Character assignation operator
+//Ice copy constructor
+//Ice assignation operator
+//Cure copy constructor
+//Cure assignation operator
+//----
+//Character copy constructor
+//Character assignation operator
+//----
+//* shoots an ice bolt at bob *
+//* heals bob's wounds *
+//----
+//* shoots an ice bolt at bob *
+//* heals bob's wounds *
+//----
+//Materia 1 unequipped.
+//* shoots an ice bolt at bob *
+//invalid materia to use.
+//* shoots an ice bolt at bob *
+//* heals bob's wounds *
