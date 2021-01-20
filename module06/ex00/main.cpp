f793bd9d3375ddb6dd900fa7e8fd550d194c20fc
qingqingqingli <kadlqbetty@gmail.com>
2021-01-20 12:58:45 +0100

    add ex00

---
+++ module06/ex00/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by qli on 20/01/2021.
+//
+
+// Examples
+// char: Non displayable
+// int: 0
+// float: 0.0f
+// double: 0.0
+//
+// ./convert nan
+// char: impossible
+// int: impossible
+// float: nanf
+// double: nan
+//
+// ./convert 42.0f
+// char:'*'
+// int: 42
+// float: 42.0f
+// double: 42.0
