bba37d29a175988b421c09238289f4352a818f51
qingqingqingli <kadlqbetty@gmail.com>
2021-01-25 13:44:01 +0100

    update ex02

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -14,7 +14,14 @@
 #include <iostream>
 
 # define RESET			"\033[0m"
+# define RED			"\033[31m"				/* Red */
+# define GREEN			"\033[32m"				/* Green */
+# define YELLOW			"\033[33m"				/* Yellow */
+# define BLUE			"\033[34m"				/* Blue */
 # define MAGENTA		"\033[35m"				/* Magenta */
+# define CYAN			"\033[36m"				/* Cyan */
+# define WHITE			"\033[37m"				/* White */
+
 
 class outOfLimits: public std::exception
 {
@@ -30,20 +37,20 @@ class Array {
 
 public:
 	Array() : _n(0), _array(){
-		std::cout << "** Array is created with [" << this->_n << "] element **" << std::endl;
+		std::cout << YELLOW << "** Array is created with [" << this->_n << "] element **" << RESET << std::endl;
 	}
 
 	// not sure if the initialization of array member is correct
 	Array(unsigned int n) : _n(n), _array(new int[_n]()){
 		for (unsigned int i = 0; i < this->_n; i++)
 			this->_array[i] = 42;
-		std::cout << "** Array is created with [" << this->_n << "] element **" << std::endl;
+		std::cout << YELLOW <<"** Array is created with [" << this->_n << "] element **" << RESET <<std::endl;
 	}
 
 	~Array() {
 		if (this->_array)
 			delete [] _array;
-		std::cout << "** Destructor is called **" << std::endl;
+		std::cout << RED << "** Destructor is called **" << RESET << std::endl;
 	}
 
 	int & operator[](unsigned int i) const {
@@ -57,15 +64,14 @@ public:
 		return this->_array[i];
 	}
 
-	// copy & assignation need to be deep copy
 	Array(Array const & src){
-		std::cout << "** Copy constructor is called **" << std::endl;
+		std::cout << YELLOW << "** Copy constructor is called **" << RESET << std::endl;
 		this->_n = 0;
 		*this = src;
 	}
 
 	Array & operator=(Array const & rhs) {
-		std::cout << "** Assignation operator is called **" << std::endl;
+		std::cout << YELLOW << "** Assignation operator is called **" << RESET << std::endl;
 		if (this != &rhs)
 		{
 			if (this->_n > 0)
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -14,13 +14,14 @@ int main(void)
 	for (unsigned int i = 0; i < b.size(); i++)
 		std::cout << b[i] << std::endl;
 
-	Array d = Array();
-	for (unsigned int i = 0; i < d.size(); i++)
-		std::cout << d[i] << std::endl;
-
 	Array e = Array(c);
 	for (unsigned int i = 0; i < e.size(); i++)
 		std::cout << e[i] << std::endl;
+
+	Array d = Array();
+	for (unsigned int i = 0; i < d.size(); i++) {
+		std::cout << d[i] << std::endl;
+	}
 
 	b[80];
 	d[80];
