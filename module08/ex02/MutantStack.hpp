8f973ad1438013d54a34f4a36f7a5e49a3747264
qingqingqingli <kadlqbetty@gmail.com>
2021-01-25 20:05:15 +0100

    update ex02

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,9 @@
+//
+// Created by qli on 25/01/2021.
+//
+
+#pragma once
+
+class MutantStack {
+
+};
---
+++ module08/ex02/main.cpp
@@ -0,0 +1,39 @@
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+int main(void)
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+
+	//[...]
+
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while(it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	return 0;
+}
