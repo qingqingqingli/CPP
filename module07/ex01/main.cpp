cd7d717aed3a43acb0240f0b9c1777683ed85470
qingqingqingli <kadlqbetty@gmail.com>
2021-01-25 14:35:09 +0100

    update ex01

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -13,19 +13,22 @@
 #include "iter.h"
 
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 };
+	int tab1[] = {0, 1, 2, 3, 4 };
 	Awesome tab2[5];
 	char tab3[6] = "98765";
+	std::string tab4[4] = {"how", "are", "you", "?"};
 
 	std::cout << "--- print ---" << std::endl;
-	iter( tab, 5, print );
-	iter( tab2, 5, print );
-	iter( tab3, 5, print );
+	iter(tab1, 5, print);
+	iter(tab2, 5, print);
+	iter(tab3, 5, print);
+	iter(tab4, 4, print);
 
 	std::cout << "--- print in color ---" << std::endl;
-	iter( tab, 5, printInColor );
-	iter( tab2, 5, printInColor );
-	iter( tab3, 5, printInColor );
+	iter(tab1, 5, printInColor);
+	iter(tab2, 5, printInColor);
+	iter(tab3, 5, printInColor);
+	iter(tab4, 4, printInColor);
 
 	return 0;
 }
@@ -45,4 +48,8 @@ int main() {
 //7
 //6
 //5
+//how
+//are
+//you
+//?
 
