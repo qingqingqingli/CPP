937d71d393a20eb561d54d7076fcf317b1619dc4
qingqingqingli <kadlqbetty@gmail.com>
2021-01-27 12:47:57 +0100

    update ex02

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -3,7 +3,63 @@
 //
 
 #pragma once
+#include <stack>
 
-class MutantStack {
+template <typename T>
+class MutantStack{
+
+public:
+	MutantStack<T>() : _stack(new std::stack<T>){
+	}
+
+	~MutantStack<T>() {
+		if (this->_stack)
+			delete this->_stack;
+	}
+
+	MutantStack<T>(MutantStack<T> const & src) {
+		*this = src;
+	}
+
+	// need to rewrite
+	MutantStack<T> & operator=(MutantStack<T> const & rhs) {
+		if (this != &rhs)
+		{
+			this->_size = rhs._size;
+			this->_stack = rhs._stack;
+		}
+		return *this;
+	}
+
+	void pop() {
+		this->_stack->pop();
+	}
+
+	void push(T t) {
+		this->_stack->push(t);
+	}
+
+	unsigned int size() const {
+		return this->_stack->size();
+	}
+
+	T	top() {
+		return this->_stack->top();
+	}
+
+	class iterator {
+
+	};
+
+	MutantStack<T>::iterator begin() {
+
+	}
+
+	MutantStack<T>::iterator end() {
+
+	}
+
+private:
+	std::stack<T> 	*_stack;
 
 };
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,28 +1,28 @@
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <stack>
 
 int main(void)
 {
 	MutantStack<int> mstack;
 
 	mstack.push(5);
-
 	mstack.push(17);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl; // returns topmost element - 17
 
-	mstack.pop();
-
-	std::cout << mstack.size() << std::endl;
+	mstack.pop(); // removes topmost element - 17
+	std::cout << mstack.top() << std::endl; // returns topmost element - 5
+	std::cout << mstack.size() << std::endl; // 1
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-
-	//[...]
-
 	mstack.push(0);
+
+	std::cout << mstack.top() << std::endl; // 0
+	std::cout << mstack.size() << std::endl; // 5
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
