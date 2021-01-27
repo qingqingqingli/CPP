5c6696d15c8f2b17feeb51a77e805830e1bac3e9
qingqingqingli <kadlqbetty@gmail.com>
2021-01-27 18:11:03 +0100

    update ex02

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -1,65 +1,55 @@
-//
-// Created by qli on 25/01/2021.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   MutantStack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qli <qli@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/27 17:39:34 by qli           #+#    #+#                 */
+/*   Updated: 2021/01/27 17:39:34 by qli           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
 #pragma once
 #include <stack>
+#include <deque>
 
-template <typename T>
-class MutantStack{
+# define RESET			"\033[0m"
+# define RED			"\033[31m"				/* Red */
+# define GREEN			"\033[32m"				/* Green */
+# define YELLOW			"\033[33m"				/* Yellow */
+# define BLUE			"\033[34m"				/* Blue */
+# define MAGENTA		"\033[35m"				/* Magenta */
+# define CYAN			"\033[36m"				/* Cyan */
+
+template<typename T, typename Container = std::deque<T> >
+class MutantStack: public std::stack<T, Container> {
 
 public:
-	MutantStack<T>() : _stack(new std::stack<T>){
-	}
-
-	~MutantStack<T>() {
-		if (this->_stack)
-			delete this->_stack;
-	}
-
-	MutantStack<T>(MutantStack<T> const & src) {
-		*this = src;
-	}
-
-	// need to rewrite
-	MutantStack<T> & operator=(MutantStack<T> const & rhs) {
+	MutantStack(): std::stack<T, Container>() {}
+	~MutantStack() {}
+	MutantStack<T, Container>(MutantStack<T, Container> const & src) {*this = src;}
+	MutantStack<T, Container> & operator=(MutantStack<T, Container> const & rhs) {
 		if (this != &rhs)
-		{
-			this->_size = rhs._size;
-			this->_stack = rhs._stack;
-		}
+			this->c = rhs.c;
 		return *this;
 	}
 
-	void pop() {
-		this->_stack->pop();
-	}
+	typedef typename Container::iterator iterator;
+	typedef typename Container::const_iterator const_iterator;
+	typedef typename Container::reverse_iterator reverse_iterator;
+	typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
-	void push(T t) {
-		this->_stack->push(t);
-	}
-
-	unsigned int size() const {
-		return this->_stack->size();
-	}
-
-	T	top() {
-		return this->_stack->top();
-	}
-
-	class iterator {
-
-	};
-
-	MutantStack<T>::iterator begin() {
-
-	}
-
-	MutantStack<T>::iterator end() {
-
-	}
-
-private:
-	std::stack<T> 	*_stack;
+	// c is the underlying container
+	iterator begin() {return this->c.begin();}
+	iterator end() {return this->c.end();}
+	const_iterator begin() const {return this->c.begin();}
+	const_iterator end() const {return this->c.end();}
+	reverse_iterator rbegin() {return this->c.rbegin();}
+	reverse_iterator rend() {return this->c.rend();}
+	const_reverse_iterator rbegin() const {return this->c.rbegin();}
+	const_reverse_iterator rend() const {return this->c.rend();}
 
 };
+
+
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -2,39 +2,53 @@
 #include "MutantStack.hpp"
 #include <iostream>
 #include <stack>
+#include <list>
 
 int main(void)
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << mstack.top() << std::endl; // returns topmost element - 17
-
-	mstack.pop(); // removes topmost element - 17
-	std::cout << mstack.top() << std::endl; // returns topmost element - 5
-	std::cout << mstack.size() << std::endl; // 1
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-
-	std::cout << mstack.top() << std::endl; // 0
-	std::cout << mstack.size() << std::endl; // 5
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
-	++it;
-	--it;
-	while(it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int> mstack;
+
+		mstack.push(1);
+		mstack.push(2);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.pop();
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.push(3);
+		mstack.push(4);
+		mstack.push(5);
+		mstack.push(6);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
 		++it;
+		--it;
+
+		while(it != ite)
+		{
+			std::cout << BLUE << *it << RESET << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << "copy size: " << s.size() << std::endl;
+		std::cout << "original size: "<< mstack.size() << std::endl;
+
+		MutantStack<int>::reverse_iterator it_reverse = mstack.rbegin();
+		MutantStack<int>::reverse_iterator ite_reverse = mstack.rend();
+
+		while(it_reverse != ite_reverse)
+		{
+			std::cout << MAGENTA << *it_reverse << RESET << std::endl;
+			++it_reverse;
+		}
 	}
-	std::stack<int> s(mstack);
 
 	return 0;
 }
